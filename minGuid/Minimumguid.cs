45b3f8de470f6f1100d959267339d898cbaefb3a
moeen.khurshid@ahrcloud.com <moeen.khurshid@ahrcloud.com>
2012-12-19 03:39:00 +0500

    Signed-off-by: moeen.khurshid@ahrcloud.com <moeen.khurshid@ahrcloud.com>

---
+++ minGuid/Minimumguid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace minGuid
+{
+    class Minimumguid
+    {
+        public string MinGuid()
+        {
+            long i = 1;
+            for (int index = 0; index < Guid.NewGuid().ToByteArray().Length; index++)
+            {
+                byte b = Guid.NewGuid().ToByteArray()[index];
+                i *= (b + 1);
+            }
+
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+    }
+}
